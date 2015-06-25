609c48157f604b549ee2dd9baa1fd51f4ce45a0c
Alexandre Betioli <apbetioli@gmail.com>
2015-06-24 23:09:29 -0300

    Corrections

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
 	
-	var input = parseInput(req);
+	var input = parseInput(req, res);
 	
 	var output = nn.run(input);
 		
@@ -18,7 +18,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function parseInput(req) {
+function parseInput(req, res) {
 	if(req.query.enemy == null || req.query.enemy.trim().length == 0) {
 		res.send('Please send "enemy" and "ally" params.<br>Ex: ally=Luna,Omniknight,Sven,Wraith King&enemy=Phantom Lancer,Slark,Pugna,Jakiro,Windranger');
 		return;
